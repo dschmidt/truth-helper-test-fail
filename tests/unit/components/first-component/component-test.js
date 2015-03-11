85448e097b14543a8e4f6c2bc11885d283aac7e5
Dominik Schmidt <dev@dominik-schmidt.de>
2015-03-11 15:49:27 +0100

    Revert "Fix test :-\"
    This reverts commit 69046410846db5f6dc64e741ed45f7291f3f5a57.

--- tests/unit/components/first-component/component-test.js
+++ tests/unit/components/first-component/component-test.js
@@ -3,20 +3,12 @@ import {
   test
 } from 'ember-qunit';
 
-import { registerHelper } from 'ember-truth-helpers/utils/register-helper';
-import {
-  orHelper
-  } from 'ember-truth-helpers/helpers/or';
-
 moduleForComponent('first-component', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
 
 test('it renders', function(assert) {
-
-  registerHelper('or',orHelper);
-
   assert.expect(2);
 
   // creates the component instance
