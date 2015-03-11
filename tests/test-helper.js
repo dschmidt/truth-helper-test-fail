766b5033434b20f2a4ef1986ac36f335ceefb002
Dominik Schmidt <dev@dominik-schmidt.de>
2015-03-11 15:50:49 +0100

    Fix test for good this time (?)

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,4 +3,18 @@ import {
   setResolver
 } from 'ember-qunit';
 
+import { registerHelper } from 'ember-truth-helpers/utils/register-helper';
+
+import { andHelper } from 'ember-truth-helpers/helpers/and';
+import { orHelper } from 'ember-truth-helpers/helpers/or';
+import { equalHelper } from 'ember-truth-helpers/helpers/equal';
+import { notHelper } from 'ember-truth-helpers/helpers/not';
+import { isArrayHelper } from 'ember-truth-helpers/helpers/is-array';
+
+registerHelper('and',andHelper);
+registerHelper('or',orHelper);
+registerHelper('eq',equalHelper);
+registerHelper('not',notHelper);
+registerHelper('is-array',isArrayHelper);
+
 setResolver(resolver);
